8f377a4b1af8863aeae46a3366e0d37c8228df66
Mislav Glas <miglas@vsite.hr>
2016-12-15 20:51:43 +0100

    Riješeni Dogadjaji Virtualne Metode

---
+++ DogadjajiVirtualneMetode/MyButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vsite.CSharp
+{
+    class MyButton : System.Windows.Forms.Button
+    {
+        protected override void OnClick(EventArgs e)
+        {
+            MessageBox.Show("Kliknuo si me!", "My button");
+            //base.OnClick(e);
+        }
+    }
+}
